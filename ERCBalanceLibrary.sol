09c851d38fed27a1ea0611f27643966d9456d904
Metaxona <110418886+Metaxona@users.noreply.github.com>
2023-07-14 12:46:42 +0800

    Update ERCBalanceLibrary.sol
    Minor Modifications on the NatSpec

--- ERCBalanceLibrary.sol
+++ ERCBalanceLibrary.sol
@@ -22,8 +22,8 @@ library ERCBalance {
 
     /**
     *   @dev ERC20 Balance Check
-    *   '_tokenAddress' - address of the ERC20
-    *   '_owner' - address of the Owner whose balance is being checked
+    *   `_tokenAddress` - address of the ERC20
+    *   `_owner` - address of the Owner whose balance is being checked
     *   returns uint256 value
     */
     function ERC20_Balance(address _tokenAddress, address _owner) public view returns(uint256) {
@@ -32,8 +32,8 @@ library ERCBalance {
 
     /**
     *   @dev ERC721 Balance Check
-    *   '_tokenAddress' - address of the ERC721
-    *   '_owner' - address of the Owner whose balance is being checked
+    *   `_tokenAddress` - address of the ERC721
+    *   `_owner` - address of the Owner whose balance is being checked
     *   returns uint256 value
     */
     function ERC721_Balance(address _tokenAddress, address _owner) public view returns(uint256) {
@@ -42,9 +42,9 @@ library ERCBalance {
 
     /**
     *   @dev ERC1155 Balance Check
-    *   '_tokenAddress' - address of the ERC1155
-    *   '_owner' - address of the Owner whose balance is being checked
-    *   'tokenId' - Id of the Token owned by _owner
+    *   `_tokenAddress` - address of the ERC1155
+    *   `_owner` - address of the Owner whose balance is being checked
+    *   `tokenId` - Id of the Token owned by _owner
     *   returns uint256 value
     */
     function ERC1155_Balance(address _tokenAddress, address _owner, uint256 tokenId) public view returns(uint256) {
@@ -55,10 +55,10 @@ library ERCBalance {
     
     /**
     *   @dev ERC20 Specified Amount Ownership Check
-    *   @notice This will Check if The '_owner' owns a certain amount of Tokens
-    *   '_tokenAddress' - address of the ERC20
-    *   '_owner' - address of the Owner whose balance is being checked
-    *   'amount' - amount of Token to be compared to the one owned by the '_owner'
+    *   @notice This will Check if The `_owner` owns a certain amount of Tokens
+    *   `_tokenAddress` - address of the ERC20
+    *   `_owner` - address of the Owner whose balance is being checked
+    *   `amount` - amount of Token to be compared to the one owned by the `_owner`
     *   returns bool value
     */
     function ownsERC20Amount(address _tokenAddress, address _owner, uint256 amount) public view returns(bool) {
@@ -67,10 +67,10 @@ library ERCBalance {
 
     /**
     *   @dev ERC721 Specified ERC721 Ownership Check
-    *   @notice This will Check if The '_owner' owns a certain ERC721 Token
-    *   '_tokenAddress' - address of the ERC721
-    *   '_owner' - address of the Owner whose balance is being checked
-    *   'tokenId' - token Id of the ERC721 to check that is supposed to be owned by the '_owner'
+    *   @notice This will Check if The `_owner` owns a certain ERC721 Token
+    *   `_tokenAddress` - address of the ERC721
+    *   `_owner` - address of the Owner whose balance is being checked
+    *   `tokenId` - token Id of the ERC721 to check that is supposed to be owned by the `_owner`
     *   returns bool value
     */
     function ownsERC721token(address _tokenAddress, address _owner, uint256 tokenId) public view returns(bool) {
@@ -79,11 +79,11 @@ library ERCBalance {
 
     /**
     *   @dev ERC1155 Specified Amount Ownership Check
-    *   @notice This will Check if The '_owner' owns a certain amount of Tokens of a specific ERC1155 Token
-    *   '_tokenAddress' - address of the ERC1155
-    *   '_owner' - address of the Owner whose balance is being checked
-    *   'tokenId' - token Id of the ERC1155 to check that is supposed to be owned by the '_owner'
-    *   'amount' - amount of 'TokenId' to be compared to the one owned by the '_owner'
+    *   @notice This will Check if The `_owner` owns a certain amount of Tokens of a specific ERC1155 Token
+    *   `_tokenAddress` - address of the ERC1155
+    *   `_owner` - address of the Owner whose balance is being checked
+    *   `tokenId` - token Id of the ERC1155 to check that is supposed to be owned by the `_owner`
+    *   `amount` - amount of `TokenId` to be compared to the one owned by the `_owner`
     *   returns bool value
     */
     function ownsERC1155Amount(address _tokenAddress, address _owner, uint256 tokenId, uint256 amount) public view returns(bool) {
@@ -95,9 +95,9 @@ library ERCBalance {
     /**
     *   @dev ERC20 Allowance Check
     *   @notice This will Check How Much Allowance The Owner Has Given To The Spender For A Specific ERC20 Token
-    *   '_tokenAddress' - address of the ERC20
-    *   '_owner' - address of the Owner whose allowance approval is being checked
-    *   'spender' - address of the contract whose being allowed to spend a certain amount of the token in place of the '_owner'
+    *   `_tokenAddress` - address of the ERC20
+    *   `_owner` - address of the Owner whose allowance approval is being checked
+    *   `spender` - address of the contract whose being allowed to spend a certain amount of the token in place of the `_owner`
     *   returns uint256 value
     */
     function ERC20Allowance(address _tokenAddress, address spender, address _owner) public view returns(uint256) {
@@ -107,11 +107,11 @@ library ERCBalance {
     /**
     *   @dev ERC721 Allowance Check
     *   @notice This will Check If The Owner Has Given An Approval For A Specific ERC721 Token To The Operator
-    *   '_tokenAddress' - address of the ERC721
-    *   'operator' - address of the contract whose being allowed to spend a certain amount of the token in place of the '_owner'
-    *   '_owner' - address of the Owner whose allowance approval is being checked
-    *   'tokenId' - token Id of the ERC721 to check that is supposed to be owned by the '_owner'
-    *   returns bool value but will revert if the '_owner' does not own any amount of the given token Id
+    *   `_tokenAddress` - address of the ERC721
+    *   `operator` - address of the contract whose being allowed to spend a certain amount of the token in place of the `_owner`
+    *   `_owner` - address of the Owner whose allowance approval is being checked
+    *   `tokenId` - token Id of the ERC721 to check that is supposed to be owned by the `_owner`
+    *   returns bool value but will revert if the `_owner` does not own any amount of the given token Id
     *   revert error is named TokenOwnerError
     */
     function ERC721Allowance(address _tokenAddress, address operator, address _owner, uint256 tokenId) public view returns(bool) {
@@ -124,10 +124,10 @@ library ERCBalance {
     /**
     *   @dev ERC721 Allowance Check
     *   @notice This will Check If The Owner Has Given An Approval For All ERC721 Token To The Operator
-    *   '_tokenAddress' - address of the ERC721
-    *   'operator' - address of the contract whose being allowed to spend a certain amount of the token in place of the '_owner'
-    *   '_owner' - address of the Owner whose allowance approval is being checked
-    *   returns bool value but will revert if the '_owner' does not own any amount of the given token Id
+    *   `_tokenAddress` - address of the ERC721
+    *   `operator` - address of the contract whose being allowed to spend a certain amount of the token in place of the `_owner`
+    *   `_owner` - address of the Owner whose allowance approval is being checked
+    *   returns bool value but will revert if the `_owner` does not own any amount of the given token Id
     */
     function ERC721AllowanceAll(address _tokenAddress, address operator, address _owner) public view returns(bool) {
         return IERC721(_tokenAddress).isApprovedForAll(_owner, operator);
@@ -136,11 +136,11 @@ library ERCBalance {
     /**
     *   @dev ERC1155 Allowance Check
     *   @notice This will Check If The Owner Has Given An Approval For All ERC1155 Token To The Operator
-    *   '_tokenAddress' - address of the ERC1155
-    *   'operator' - address of the contract whose being allowed to spend a certain amount of the token in place of the '_owner'
-    *   '_owner' - address of the Owner whose allowance approval is being checked
-    *   'tokenId' - token Id of the ERC721 to check that is supposed to be owned by the '_owner'
-    *   returns bool value but will revert if the '_owner' does not own any amount of the given token Id
+    *   `_tokenAddress` - address of the ERC1155
+    *   `operator` - address of the contract whose being allowed to spend a certain amount of the token in place of the `_owner`
+    *   `_owner` - address of the Owner whose allowance approval is being checked
+    *   `tokenId` - token Id of the ERC721 to check that is supposed to be owned by the `_owner`
+    *   returns bool value but will revert if the `_owner` does not own any amount of the given token Id
     */
     function ERC1155Allowance(address _tokenAddress, address operator, address _owner) public view returns(bool) {
         return IERC1155(_tokenAddress).isApprovedForAll(_owner, operator);
@@ -151,10 +151,10 @@ library ERCBalance {
     /**
     *   @dev ERC20 Specified Amount Allowance Check
     *   @notice This will Check How Much Allowance The Owner Has Given To The Spender For A Specific ERC20 Token
-    *   '_tokenAddress' - address of the ERC20
-    *   'spender' - address of the contract whose being allowed to spend a certain amount of the token in place of the '_owner'
-    *   '_owner' - address of the Owner whose allowance approval is being checked
-    *   'amount' - amount of ERC20 to be compared to the one allowed by the '_owner'
+    *   `_tokenAddress` - address of the ERC20
+    *   `spender` - address of the contract whose being allowed to spend a certain amount of the token in place of the `_owner`
+    *   `_owner` - address of the Owner whose allowance approval is being checked
+    *   `amount` - amount of ERC20 to be compared to the one allowed by the `_owner`
     *   returns bool value
     */
     function hasEnoughERC20Allowance(address _tokenAddress, address spender, address _owner, uint256 amount) public view returns(bool) {
@@ -164,11 +164,11 @@ library ERCBalance {
     /**
     *   @dev ERC721 Specified Allowance Check
     *   @notice This will Check If The Owner Has Given An Approval For All ERC721 Token To The Operator
-    *   '_tokenAddress' - address of the ERC721
-    *   'operator' - address of the contract whose being allowed to spend a certain amount of the token in place of the '_owner'
-    *   '_owner' - address of the Owner whose allowance approval is being checked
-    *   'tokenId' - [use 0 if you are sure that it is approved by all] token Id of the ERC721 to check that is supposed to be owned by the '_owner'
-    *   returns bool value but will revert if the '_owner' does not own any amount of the given token Id
+    *   `_tokenAddress` - address of the ERC721
+    *   `operator` - address of the contract whose being allowed to spend a certain amount of the token in place of the `_owner`
+    *   `_owner` - address of the Owner whose allowance approval is being checked
+    *   `tokenId` - [use 0 if you are sure that it is approved by all] token Id of the ERC721 to check that is supposed to be owned by the `_owner`
+    *   returns bool value but will revert if the `_owner` does not own any amount of the given token Id
     *   revert error is named TokenOwnerError
     */
     function hasEnoughERC721Allowance(address _tokenAddress, address operator, address _owner, uint256 tokenId) public view returns(bool) {
@@ -181,12 +181,12 @@ library ERCBalance {
     /**
     *   @dev ERC1155 Specified Allowance Check
     *   @notice This will Check If The Owner Has Given An Approval For All ERC1155 Token To The Operator
-    *   @notice This is The Same As The 'ERC1155Allowance' Function Since A Standard ERC1155 Does Not Have Another Allowance Function Aside From isApprovedForAll 
-    *   '_tokenAddress' - address of the ERC1155
-    *   'operator' - address of the contract whose being allowed to spend a certain amount of the token in place of the '_owner'
-    *   '_owner' - address of the Owner whose allowance approval is being checked
-    *   'tokenId' - token Id of the ERC1155 to check that is supposed to be owned by the '_owner'
-    *   returns bool value but will revert if the '_owner' does not own any amount of the given token Id
+    *   @notice This is The Same As The `ERC1155Allowance` Function Since A Standard ERC1155 Does Not Have Another Allowance Function Aside From isApprovedForAll 
+    *   `_tokenAddress` - address of the ERC1155
+    *   `operator` - address of the contract whose being allowed to spend a certain amount of the token in place of the `_owner`
+    *   `_owner` - address of the Owner whose allowance approval is being checked
+    *   `tokenId` - token Id of the ERC1155 to check that is supposed to be owned by the `_owner`
+    *   returns bool value but will revert if the `_owner` does not own any amount of the given token Id
     */
     function hasEnoughERC1155Allowance(address _tokenAddress, address operator, address _owner) public view returns(bool) {
         return IERC1155(_tokenAddress).isApprovedForAll(_owner, operator);
@@ -197,11 +197,11 @@ library ERCBalance {
     /**
     *   @dev ERC20 Balance And Allowance Requirements Check
     *   @notice This will Check If The Owner Has Given Enough ERC20 Token To The Spender and Has Enough ERC20 balance
-    *   '_tokenAddress' - address of the ERC20
-    *   'operator' - address of the contract whose being allowed to spend a certain amount of the token in place of the '_owner'
-    *   '_owner' - address of the Owner whose allowance approval is being checked
-    *   'amount' - amount of ERC20 to be compared to the one owned by the '_owner'
-    *   returns bool value but will revert if the '_owner' does not own any amount of the given token Id
+    *   `_tokenAddress` - address of the ERC20
+    *   `operator` - address of the contract whose being allowed to spend a certain amount of the token in place of the `_owner`
+    *   `_owner` - address of the Owner whose allowance approval is being checked
+    *   `amount` - amount of ERC20 to be compared to the one owned by the `_owner`
+    *   returns bool value but will revert if the `_owner` does not own any amount of the given token Id
     *   revert error is named TokenOwnerError
     */
     function ERC20RequirementsTrue(address _tokenAddress, address spender, address _owner, uint256 amount) public view returns(bool) {
@@ -211,11 +211,11 @@ library ERCBalance {
     /**
     *   @dev ERC721 Balance And Allowance Requirements Check
     *   @notice This will Check If The Owner Has Given An Approval For All ERC721 Token To The Operator and Has Enough ERC721 balance
-    *   '_tokenAddress' - address of the ERC721
-    *   'operator' - address of the contract whose being allowed to spend a certain amount of the token in place of the '_owner'
-    *   '_owner' - address of the Owner whose allowance approval is being checked
-    *   'tokenId' - token Id of the ERC721 to check that is supposed to be owned by the '_owner'
-    *   returns bool value but will revert if the '_owner' does not own any amount of the given token Id
+    *   `_tokenAddress` - address of the ERC721
+    *   `operator` - address of the contract whose being allowed to spend a certain amount of the token in place of the `_owner`
+    *   `_owner` - address of the Owner whose allowance approval is being checked
+    *   `tokenId` - token Id of the ERC721 to check that is supposed to be owned by the `_owner`
+    *   returns bool value but will revert if the `_owner` does not own any amount of the given token Id
     *   revert error is named TokenOwnerError
     */
     function ERC721RequirementsTrue(address _tokenAddress, address operator, address _owner, uint256 tokenId) public view returns(bool) {
@@ -225,11 +225,11 @@ library ERCBalance {
     /**
     *   @dev ERC1155 Balance And Allowance Requirements Check
     *   @notice This will Check If The Owner Has Given An Approval For All ERC1155 Token To The Operator and Has Enough ERC1155 balance
-    *   '_tokenAddress' - address of the ERC1155
-    *   'operator' - address of the contract whose being allowed to spend a certain amount of the token in place of the '_owner'
-    *   '_owner' - address of the Owner whose allowance approval is being checked
-    *   'tokenId' - token Id of the ERC1155 to check that is supposed to be owned by the '_owner'
-    *   'amount' - amount of 'TokenId' to be compared to the one owned by the '_owner'
+    *   `_tokenAddress` - address of the ERC1155
+    *   `operator` - address of the contract whose being allowed to spend a certain amount of the token in place of the `_owner`
+    *   `_owner` - address of the Owner whose allowance approval is being checked
+    *   `tokenId` - token Id of the ERC1155 to check that is supposed to be owned by the `_owner`
+    *   `amount` - amount of `TokenId` to be compared to the one owned by the `_owner`
     *   returns bool value 
     */
     function ERC1155RequirementsTrue(address _tokenAddress, address operator, address _owner, uint256 tokenId, uint256 amount) public view returns(bool) {
